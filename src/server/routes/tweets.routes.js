027ca8d65cfbefb8a5bc58eda240e45e47c77853
claudiocfls <claudiosilva.cfls@gmail.com>
2020-02-05 22:54:15 -0300

    chore: use auth middleware on tweets routes

--- src/server/routes/tweets.routes.js
+++ src/server/routes/tweets.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const routes = express.Router();
+const auth = require('./../../server/middlewares/auth');
 
 const TweetsController = require('../../controllers/tweets.controller');
 
+routes.use('/tweets', auth());
 routes.get('/tweets', TweetsController.getAll);
 routes.post('/tweets',TweetsController.create);
 routes.delete('/tweets/:id', (req, res) => { res.send("rota de delecao")});
